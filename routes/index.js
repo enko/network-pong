de8279e17ad2d87c2fb23dcb3a31880fcbee2e70
Tim Schumacher <tim@bandenkrieg.hacked.jp>
2013-05-14 20:29:32 +0200

    Added a SessionID for the game.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
+var crypto = require('crypto');
 
 /*
  * GET home page.
  */
+
+function getSessionID() {
+  return crypto.createHash('sha256').update((new Date()).toString()).digest('hex');
+}
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
 exports.game = function(req, res){
-  res.render('game', { title: 'Express' });
+  res.render('game', { title: 'Express', sessionID: getSessionID });
 };
